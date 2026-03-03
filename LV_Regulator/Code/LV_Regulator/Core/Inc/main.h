0d0320c7bed6af4ed2b408bd8e04af697a53ba48
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2026-03-03 15:18:03 +0100

    LV_Regulator Control changes
    Added debug and development equipment, added config. eeprom, more...

--- LV_Regulator/Code/LV_Regulator/Core/Inc/main.h
+++ LV_Regulator/Code/LV_Regulator/Core/Inc/main.h
@@ -59,6 +59,30 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define USART1_DIR_Pin GPIO_PIN_13
 #define USART1_DIR_GPIO_Port GPIOC
+#define DIAG_OK_Pin GPIO_PIN_2
+#define DIAG_OK_GPIO_Port GPIOA
+#define DIAG_ERR_Pin GPIO_PIN_3
+#define DIAG_ERR_GPIO_Port GPIOA
+#define RELEJ_Pin GPIO_PIN_0
+#define RELEJ_GPIO_Port GPIOB
+#define BOOT1_Pin GPIO_PIN_2
+#define BOOT1_GPIO_Port GPIOB
+#define ADR1_Pin GPIO_PIN_11
+#define ADR1_GPIO_Port GPIOB
+#define ADR2_Pin GPIO_PIN_12
+#define ADR2_GPIO_Port GPIOB
+#define ADR3_Pin GPIO_PIN_13
+#define ADR3_GPIO_Port GPIOB
+#define ADR4_Pin GPIO_PIN_14
+#define ADR4_GPIO_Port GPIOB
+#define ADR5_Pin GPIO_PIN_15
+#define ADR5_GPIO_Port GPIOB
+#define DIAG_RUN_Pin GPIO_PIN_8
+#define DIAG_RUN_GPIO_Port GPIOA
+#define DIAG_TX_Pin GPIO_PIN_9
+#define DIAG_TX_GPIO_Port GPIOA
+#define DIAG_RX_Pin GPIO_PIN_10
+#define DIAG_RX_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
--- LV_Regulator/Code/LV_Regulator/Core/Src/main.c
+++ LV_Regulator/Code/LV_Regulator/Core/Src/main.c
@@ -45,6 +45,8 @@ ADC_HandleTypeDef hadc2;
 
 I2C_HandleTypeDef hi2c1;
 
+SPI_HandleTypeDef hspi1;
+
 UART_HandleTypeDef huart1;
 
 PCD_HandleTypeDef hpcd_USB_FS;
@@ -61,6 +63,7 @@ static void MX_ADC2_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USB_PCD_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -104,6 +107,7 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   MX_USB_PCD_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -295,6 +299,44 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_HARD_OUTPUT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -379,12 +421,44 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(USART1_DIR_GPIO_Port, USART1_DIR_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DIAG_OK_Pin|DIAG_ERR_Pin|DIAG_RUN_Pin|DIAG_TX_Pin
+                          |DIAG_RX_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, RELEJ_Pin|ADR1_Pin|ADR2_Pin|ADR3_Pin
+                          |ADR4_Pin|ADR5_Pin, GPIO_PIN_RESET);
+
   /*Configure GPIO pin : USART1_DIR_Pin */
   GPIO_InitStruct.Pin = USART1_DIR_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(USART1_DIR_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIAG_OK_Pin DIAG_ERR_Pin DIAG_RUN_Pin DIAG_TX_Pin
+                           DIAG_RX_Pin */
+  GPIO_InitStruct.Pin = DIAG_OK_Pin|DIAG_ERR_Pin|DIAG_RUN_Pin|DIAG_TX_Pin
+                          |DIAG_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RELEJ_Pin ADR1_Pin ADR2_Pin ADR3_Pin
+                           ADR4_Pin ADR5_Pin */
+  GPIO_InitStruct.Pin = RELEJ_Pin|ADR1_Pin|ADR2_Pin|ADR3_Pin
+                          |ADR4_Pin|ADR5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BOOT1_Pin */
+  GPIO_InitStruct.Pin = BOOT1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(BOOT1_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
